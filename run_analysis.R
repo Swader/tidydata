4cf25f22955a84f80700f64b1773cfc8f04f77d6
Bruno Skvorc <bruno@skvorc.me>
2015-04-27 00:17:21 +0200

    Made some changes

--- run_analysis.R
+++ run_analysis.R
@@ -1,30 +1,29 @@
-subjectTrain <- read.table("UCI HAR Dataset/train/subject_train.txt")
-subjectTest <- read.table("UCI HAR Dataset/test/subject_test.txt")
-xTrain <- read.table("UCI HAR Dataset/train/X_train.txt")
-xTest <- read.table("UCI HAR Dataset/test/X_test.txt")
-yTrain <- read.table("UCI HAR Dataset/train/y_train.txt")
-yTest <- read.table("UCI HAR Dataset/test/y_test.txt")
+# Make sure you have dplyr installed to use this script!
+
 featuresNames <- read.table("UCI HAR Dataset/features.txt")
 activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
 
+subjectTest <- read.table("UCI HAR Dataset/test/subject_test.txt")
+xTest <- read.table("UCI HAR Dataset/test/X_test.txt")
+names(xTest) <- featuresNames$V2
+yTest <- read.table("UCI HAR Dataset/test/y_test.txt")
+test <- cbind(subjectTest, yTest, xTest)
+names(test)[1:2] <- c('subject', 'activity')
 
-# names(subjectTrain)[1] <-  "subjectId"
-# names(subjectTest)[1] <- "subjectId"
-# names(yTrain)[1] <- "activity"
-# names(yTest)[1] <- "activity"
-# 
-# names(xTrain) <- featuresNames$V2
-# names(xTest) <- featuresNames$V2
-# 
-# joinedTrain <- cbind(subjectTrain,yTrain,xTrain)
-# joinedTest <- cbind(subjectTest,yTest,xTest)
-# joined <- rbind(joinedTrain,joinedTest)
-# 
-# keepCols <- grep("mean\\(\\)|std\\(\\)|subjectId|activity",names(joined))
-# joined <- joined[,keepCols]
-# 
-# joined$activity <- as.factor(joined$activity)
-# levels(joined$activity) <- activityLabels$V2
-# 
-# tidy <- joined %>% group_by(subjectId,activity) %>% summarise_each(funs(mean))
-# write.csv(tidy, "tidy.txt", row.names=FALSE)
+subjectTrain <- read.table("UCI HAR Dataset/train/subject_train.txt")
+xTrain <- read.table("UCI HAR Dataset/train/X_train.txt")
+names(xTrain) <- featuresNames$V2
+yTrain <- read.table("UCI HAR Dataset/train/y_train.txt")
+train <- cbind(subjectTrain, yTrain, xTrain)
+names(train)[1:2] <- c('subject', 'activity')
+
+joined <- rbind(test, train)
+
+keepCols <- grep("mean\\(\\)|std\\(\\)|subject|activity",names(joined))
+joined <- joined[,keepCols]
+
+joined$activity <- as.factor(joined$activity)
+levels(joined$activity) <- activityLabels$V2
+
+tidy <- joined %>% group_by(subject,activity) %>% summarise_each(funs(mean))
+write.csv(tidy, "tidy.txt", row.names=FALSE)
